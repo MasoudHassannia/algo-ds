04327389ab62985f33d226744c2ec78f85f00175
Masoud Hassannia <m.hasan.nia@sain.ir>
2023-02-10 21:47:30 +0330

    implement Insertion Sort

--- src/org/alog_ds/practice/Main.java
+++ src/org/alog_ds/practice/Main.java
@@ -1,12 +1,13 @@
 package org.alog_ds.practice;
 
 import org.alog_ds.practice.sort.MyBubbleSort;
+import org.alog_ds.practice.sort.MyInsertionSort;
 import org.alog_ds.practice.sort.MySelectionSort;
 
 public class Main {
     public static void main(String[] args) {
 
         int[] arr = {4,1,7,-12,33,2,7};
-        MySelectionSort.selectionSort(arr);
+        MyInsertionSort.insertionSort(arr);
     }
 }
---
+++ src/org/alog_ds/practice/sort/MyInsertionSort.java
@@ -0,0 +1,21 @@
+package org.alog_ds.practice.sort;
+
+public class MyInsertionSort {
+
+    public static void insertionSort(int[] arrays){
+
+        for (int i =1;i < arrays.length;i++) {
+            int smallElement = arrays[i];
+
+            int j;
+            for(j = i; j > 0 && arrays[j-1] > smallElement;j--)
+                arrays[j] = arrays[j-1];
+
+            arrays[j] = smallElement;
+        }
+
+        for (int array : arrays) {
+            System.out.println(array);
+        }
+    }
+}
